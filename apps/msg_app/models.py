03db512f882a34c9096dccdc9c4074f73e47fa1a
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-16 19:55:09 -0800

    added message functionality

--- apps/msg_app/models.py
+++ apps/msg_app/models.py
@@ -5,9 +5,35 @@ from django.db import models
 from ..lr_app.models import Users
 
 # Create your models here.
+class MessagesManager(models.Manager):
+    """
+    Manages requests from the server and accordingly queries the database
+
+    Attributes:
+        addMessage: adds a message to the database
+    """
+    
+    def addMessage(self, data, u_id):
+        """
+        Adds a message to the database
+
+        Args:
+            u_id (int): unique id of the user
+            data (request.POST): request
+        """
+        response = []
+        if not data['message']:
+            response.append('Message cannot be empty!')
+        if len(response) == 0:
+            Users.objects.get(id=u_id).messages.add(
+                Messages.objects.create(message=data['message'])
+            )
+        return response
+
 class Messages(models.Model):
     message = models.TextField()
     user = models.ForeignKey(Users, related_name='messages')
+    objects = MessagesManager()
 
 class Comments(models.Model):
     comment = models.TextField()
--- apps/msg_app/views.py
+++ apps/msg_app/views.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
 
+from ..lr_app.models import Users
+from models import Messages, Comments
+from django.contrib import messages
 # Create your views here.
 def add_message(request, u_id):
     """
     Route for processing a message into a user
     """
-    return HttpResponse('add a message for ' + str(u_id))
+    if request.method == 'POST':
+        response = Messages.objects.addMessage(request.POST, u_id)
+        if len(response) != 0:
+            for message in response:
+                messages.warning(request, message)
+    return redirect('/users/show/'+str(u_id))
 
 def post_comment(request, u_id, m_id):
     """
